3bfc221d7461466ee502dee9aca7641f6b0e6b58
Vineet Tiwari <166735997+vineettiwari1708@users.noreply.github.com>
2025-09-10 13:09:42 +0530

    Update header.php

--- header.php
+++ header.php
@@ -1,15 +1,10 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?php wp_title(); ?></title>
     <?php wp_head(); ?>
 </head>
-<body <?php body_class(); ?>>
-<header class="site-header">
-    <h1 class="site-title">
-        <a href="<?php echo esc_url(home_url('/')); ?>">
-            <?php bloginfo('name'); ?>
-        </a>
-    </h1>
-</header>
+<body <?php body_class(); ?> data-page="<?php echo get_post_field('post_name', get_post()); ?>">
+
