c6497b8733b2427a50317981761960331f2b8224
Vineet Tiwari <166735997+vineettiwari1708@users.noreply.github.com>
2025-09-10 13:10:31 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,11 @@
 <?php get_header(); ?>
 
-<div class="content">
-    <h1><?php the_title(); ?></h1>
-    <div class="entry-content">
-        <?php
-        if (have_posts()) :
-            while (have_posts()) : the_post();
-                the_content();
-            endwhile;
-        else :
-            echo '<p>No content found.</p>';
-        endif;
-        ?>
-    </div>
-</div>
+<!-- 
+  WordPress normally outputs content here, but
+  since layout is fully controlled remotely,
+  you can leave this blank or add a fallback message
+-->
+
+<div id="root"></div>
 
 <?php get_footer(); ?>
